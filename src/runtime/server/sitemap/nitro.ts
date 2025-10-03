909ce764bc624d656750fb05c6680ba0901176e7
silverbackdan <daniel@silverback.is>
2025-10-03 18:50:24 +0100

    Logging validity checking

--- src/runtime/server/sitemap/nitro.ts
+++ src/runtime/server/sitemap/nitro.ts
@@ -57,6 +57,9 @@ async function buildSitemapXml(event: H3Event, definition: SitemapDefinition, re
   const routeRuleMatcher = createNitroRouteRuleMatcher()
   const { autoI18n } = runtimeConfig
 
+  // eslint-disable-next-line no-console
+  console.log('#1 all URLs', sitemapUrls)
+
   // Process in place to avoid creating intermediate arrays
   let validCount = 0
   for (const u of sitemapUrls) {
@@ -96,6 +99,9 @@ async function buildSitemapXml(event: H3Event, definition: SitemapDefinition, re
   // Truncate array to valid entries only
   sitemapUrls.length = validCount
 
+  // eslint-disable-next-line no-console
+  console.log('#2 valid count', validCount, sitemapUrls)
+
   // 6. nitro hooks
   const locSize = sitemapUrls.length
   const resolvedCtx: SitemapRenderCtx = {
@@ -108,27 +114,17 @@ async function buildSitemapXml(event: H3Event, definition: SitemapDefinition, re
   // we need to normalize any new urls otherwise they won't appear in the final sitemap
   // Note this is risky and users should be using the sitemap:input hook for additions
 
-  // eslint-disable-next-line no-console
-  console.log('#1 resolved...', resolvedCtx.urls, 'locSize', locSize)
-
   if (resolvedCtx.urls.length !== locSize) {
     resolvedCtx.urls = resolvedCtx.urls.map(e => preNormalizeEntry(e, resolvers))
   }
 
   const maybeSort = (urls: ResolvedSitemapUrl[]) => runtimeConfig.sortEntries ? sortInPlace(urls) : urls
 
-  // eslint-disable-next-line no-console
-  console.log('#2 before normalized', resolvedCtx.urls)
   const normalizedPreDedupe = resolvedCtx.urls.map(e => normaliseEntry(e, definition.defaults, resolvers))
 
-  // eslint-disable-next-line no-console
-  console.log('#3 normalizedPreDedupe', normalizedPreDedupe)
   const urls = maybeSort(
     mergeOnKey(normalizedPreDedupe, '_key').map(e => normaliseEntry(e, definition.defaults, resolvers))
   )
-
-  // eslint-disable-next-line no-console
-  console.log('-- FINAL urls deduped --', urls)
 
   // Check if this is a chunk request that would be empty
   if (definition._isChunking && definition.sitemapName.includes('-')) {
